eb9099d40547d42b58b49d10965dee5f0db7a17e
Oshanlakshith <oshanlakshitha146@gmail.com>
2022-11-07 08:46:35 +0530

    secound page

---
+++ src/app/secound-page/secound-page.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SecoundPageComponent } from './secound-page.component';
+
+describe('SecoundPageComponent', () => {
+  let component: SecoundPageComponent;
+  let fixture: ComponentFixture<SecoundPageComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SecoundPageComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(SecoundPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
