b3d80e2715be7a99fc57716b56e85b78708d1207
Oshanlakshith <oshanlakshitha146@gmail.com>
2022-11-08 15:06:07 +0530

    validate and loging form complete

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,6 +14,7 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
+
   });
 
   it(`should have as title 'angular-tour-of-heroes'`, () => {
@@ -28,4 +29,6 @@ describe('AppComponent', () => {
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('.content span')?.textContent).toContain('angular-tour-of-heroes app is running!');
   });
+ 
 });
+
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,20 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  
 })
+
 export class AppComponent {
   title = 'angular-tour-of-heroes';
+   hellow(){
+var email=(<HTMLInputElement>document.getElementById("emails")).value;
+
+var password=(<HTMLInputElement>document.getElementById("password")).value;
+
+if(email=="oshanlakshitha146@gmail.com" && password=="12345"){
+alert("Confirm")
+}else{
+alert("loging Fail") 
+}
+}
 }
